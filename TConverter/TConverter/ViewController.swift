8f7572fa4244b8861b9458c0eeb33c3aa0d393d0
Nazar Dranhovskyi <ndranhovskyi@oneviewhealthcare.com>
2021-08-05 23:21:12 +0300

    Finish Celsius to Farenheit project

--- TConverter/TConverter/ViewController.swift
+++ TConverter/TConverter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var farenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let tepretureCelcius = Int(round(sender.value))
+        celsiusLabel.text = "\(tepretureCelcius)ºC"
+        let tempretureFarenheit = (tepretureCelcius * 9/5) + 32
+        farenheitLabel.text = "\(tempretureFarenheit)ºF"
+    }
+    
 }
 
