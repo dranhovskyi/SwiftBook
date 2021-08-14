cc4435bf4089374941817aba87858870d1d3b0f1
Nazar Dranhovskyi <ndranhovskyi@oneviewhealthcare.com>
2021-08-14 22:41:00 +0300

    UISwitch

--- UIControls/UIScrollView/UIScrollView/ViewController.swift
+++ UIControls/UIScrollView/UIScrollView/ViewController.swift
@@ -11,6 +11,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var labelControl: UILabel!
     @IBOutlet weak var datePickerControl: UIDatePicker!
+    @IBOutlet weak var hideElemetsLabel: UILabel!
+    @IBOutlet weak var sliderControl: UISlider!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var textFieldControl: UITextField!
+    @IBOutlet weak var buttonControl: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +28,20 @@ class ViewController: UIViewController {
         dateFormatter.dateStyle = .full
         dateFormatter.locale = Locale(identifier: "ru_RU")
         labelControl.text = dateFormatter.string(from: sender.date)
+    }
+    
+    @IBAction func hideElemets(_ sender: UISwitch) {
+        segmentControl.isHidden = !segmentControl.isHidden
+        labelControl.isHidden = !labelControl.isHidden
+        datePickerControl.isHidden = !datePickerControl.isHidden
+        buttonControl.isHidden = !buttonControl.isHidden
+        sliderControl.isHidden = !sliderControl.isHidden
+        textFieldControl.isHidden = !textFieldControl.isHidden
+        if (sender.isOn) {
+            hideElemetsLabel.text = "Отобразить все елементы"
+        } else {
+            hideElemetsLabel.text = "Скрыть все елементы"
+        }
     }
     
 }
