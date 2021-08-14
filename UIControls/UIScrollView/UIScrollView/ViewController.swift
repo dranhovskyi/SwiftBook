2b89fc5a4c4600f04bccac5f5579259340b5af1b
Nazar Dranhovskyi <ndranhovskyi@oneviewhealthcare.com>
2021-08-14 21:35:49 +0300

    UIDatePicker

--- UIControls/UIScrollView/UIScrollView/ViewController.swift
+++ UIControls/UIScrollView/UIScrollView/ViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelControl: UILabel!
+    @IBOutlet weak var datePickerControl: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        labelControl.numberOfLines = 2
     }
     
+    @IBAction func dateAction(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        labelControl.text = dateFormatter.string(from: sender.date)
+    }
     
 }
 
