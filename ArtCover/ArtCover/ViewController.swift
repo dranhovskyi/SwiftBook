50d78a2c0637586273b0eb993dbe9c3c0194130b
Nazar Dranhovskyi <ndranhovskyi@oneviewhealthcare.com>
2021-08-29 21:33:03 +0300

    UITableView

---
+++ ArtCover/ArtCover/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtCover
+//
+//  Created by Nazar Dranhovskyi on 29.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
